b597400934f87d81711ad4282c1335527f7a1368
vanMjnh <147973559+vandmjnh@users.noreply.github.com>
2024-06-11 16:03:01 +0700

    Delete 17_ExceptionHandling directory

--- 17_ExceptionHandling/exceptionHandling.py
+++
@@ -1,5 +0,0 @@
-names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
-# Unpack the first five countries and store them in a variable nordic_countries, store Estonia and Russia in es, and ru respectively.
-*nordic_countries, es, ru = names
-print(nordic_countries, es, ru)
-
